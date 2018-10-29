084b30431d35a16e1969f5795f44003508984c1f
MuathAlawi Alawi <muathalawi1@gmail.com>
2018-10-29 02:15:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function distanceFromHqInBlocks(currentBlock) {
   // How far are they going?
  
  const numberOfBlocks = Math.abs(block1 - block2)
-  const numberOfBlocks
+  const numberOfFeet = numberOfBlocks * feetInBlock
  
   // first four hundred are free
  }
